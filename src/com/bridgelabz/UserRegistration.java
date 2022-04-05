cae9a53ae1c8038d8c9c6bcc42d404c2a7e8ac06
reshsingh1995@gmail.com <reshsingh1995@gmail.com>
2022-04-05 12:45:37 +0530

    Added first name validator

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,8 +1,19 @@
 package com.bridgelabz;
 
+import java.util.regex.Pattern;
+
+interface Validation {
+    public String validate(String regex, String userDetails);
+}
+
 public class UserRegistration {
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Problem using lambda");
+
+        Validation isValidFirstName = (pattern, firstName) -> {
+            return "First Name is " + Pattern.compile(pattern).matcher(firstName).matches();
+        };
+        System.out.println(isValidFirstName.validate("^[A-Z]{1}[a-z]{2,}$", "Resh"));
     }
 }
